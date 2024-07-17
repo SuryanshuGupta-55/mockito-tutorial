0d294f9d393acce0d824c010b2bb9a3749e1df47
SuryanshuGupta-55 <suryanshuguptaofficial@gmail.com>
2024-07-17 22:44:58 +0530

    Added another test method for mockito.

--- src/test/java/com/learning/mockito/mockito_tutorial/buisness/SomeBusinessMockImplTest.java
+++ src/test/java/com/learning/mockito/mockito_tutorial/buisness/SomeBusinessMockImplTest.java
@@ -18,4 +18,14 @@ public class SomeBusinessMockImplTest {
         int result = someBusiness.findTheGreatestFromAllData();
         assertEquals(25, result);
     }
+    @Test
+    void test2(){
+        DataService dataServiceMock = mock(DataService.class);
+        when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {14});
+        // Basically, we are mocking the DataService interface and the line 15 tells what to do when we retrieve data from the interface mock.
+        SomeBuisnessImpl someBusiness = new SomeBuisnessImpl(dataServiceMock);
+        int result = someBusiness.findTheGreatestFromAllData();
+        assertEquals(14, result);
+    }
+    //Here, we can simply copy the method for another test-case.
 }
